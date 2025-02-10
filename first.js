85a0e636121cc8101112234ec3e8204e814bba83
SurjithHanji <surjithhanji12345@gmail.com>
2025-02-10 13:39:21 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,19 +1,34 @@
-let a= prompt("enter the value of a:")
-n=Number.parseInt(a)
-let i=0
-while(i<a){
-    console.log(i)
-    i++
+//problem 1
+let obj={harry:90,
+    lav:98,
+    moni:78
+}
+for(let i=0;i<Object.keys(obj).length;i++){
+    console.log("the marks of "+Object.keys(obj)[i]+"are"+obj[Object.keys(obj)[i]])
 }
 
-//do while loop
+//problem 2
+let marks={harry:90,
+    lav:98,
+    moni:78
+}
+for ( let key in marks){
+    console.log("the marks of"+key+"are"+marks[key])
+}
 
+//problem 3
 
-let a= prompt("enter the value of a:")
-n=Number.parseInt(a)
-let i=0
+let cn=45
+let i
+while(i != cn){
+    i = prompt("enter the number:")
+    console.log("try again")
+}
+console.log("you have entered correct number")
 
-do{
-    console.log(i)
-    i++
-}while(i<n)
+//problem 4
+ 
+const mean=(a,b,c,d,e)=>{
+    return (a+b+c+d+e)/4
+}
+console.log(mean(4,5,6,8,7))
