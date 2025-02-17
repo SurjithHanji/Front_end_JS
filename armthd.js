236309caa5f1fabd416972e9f2198417ed4e1a7c
SurjithHanji <surjithhanji12345@gmail.com>
2025-02-17 23:12:43 +0530

    Add files via upload

---
+++ armthd.js
@@ -0,0 +1,18 @@
+let num=[1,3,4,5,6,8]
+let b=num.toString()
+console.log(b)
+let c=num.join("_")
+console.log(c)
+
+let d=num.pop()
+console.log(d)
+
+let e=num.push(100)//return the new array length
+console.log(num,e)
+
+
+let r=num.shift()
+console.log(r)
+
+let s=num.unshift(99)
+console.log(s)
