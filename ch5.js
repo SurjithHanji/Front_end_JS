18014d73c7c91d1d48666498624fb0c78d94c4ea
SurjithHanji <surjithhanji12345@gmail.com>
2025-02-17 23:13:02 +0530

    Add files via upload

---
+++ ch5.js
@@ -0,0 +1,20 @@
+let a=[1,false,"hey",4,5]
+console.log(a)
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+
+console.log(a.length)
+
+
+a[0]=true
+console.log(a)
+
+//arrys are mutable
+
+
+for(let i=0;i<a.length;i++){
+    console.log(a[i])
+}
