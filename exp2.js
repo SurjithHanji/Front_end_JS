5402cb138aaf5e9da3695feb9d2ab87b889e8584
SurjithHanji <surjithhanji12345@gmail.com>
2025-02-26 18:50:10 +0530

    Add files via upload

---
+++ exp2.js
@@ -0,0 +1,6 @@
+let a=document.body.firstElementChild
+console.log(a)
+console.log(a.rows)
+console.log(a.tHead)
+console.log(a.tHead.firstElementChild)
+console.log(a.rows[2])
---
+++ script.js
@@ -0,0 +1,11 @@
+// const changeBodyRed=()=>{
+//     document.body.firstElementChild.style.background="red"
+// }
+
+
+let b=document.body
+console.log("first child of b is:",b.firstChild)
+console.log("first element child of b is:",b.firstElementChild)
+console.log("previous sibling is:",b.previousSibling)
+console.log(b.nextSibling)
+console.log(b.lastElementChild)
